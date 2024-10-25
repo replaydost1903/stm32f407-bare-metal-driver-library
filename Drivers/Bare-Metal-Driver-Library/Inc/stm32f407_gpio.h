b649a0cfe8ead60322738e7c47d8d33a610701cf
Kubilay Közleme <burhankozleme@gmail.com>
2024-10-25 21:54:53 +0300

    added gpio header file for stm32f407 library

---
+++ Drivers/Bare-Metal-Driver-Library/Inc/stm32f407_gpio.h
@@ -0,0 +1,19 @@
+#ifndef BARE_METAL_DRIVER_LIBRARY_INC_STM32F407_GPIO_H_
+#define BARE_METAL_DRIVER_LIBRARY_INC_STM32F407_GPIO_H_
+
+#include "stm32f4xx.h"
+
+/*
+ *  GPIO Modes
+ */
+#define GPIO_MODE_IN		(0U)
+#define GPIO_MODE_OUT		(1U)
+#define GPIO_MODE_AF		(2U)
+#define GPIO_MODE_AN		(3U)
+
+
+
+
+
+
+#endif /* BARE_METAL_DRIVER_LIBRARY_INC_STM32F407_GPIO_H_ */
