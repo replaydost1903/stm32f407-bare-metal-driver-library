a8d528036f35f8990abc29b106d0fb9994f599a6
Kubilay Közleme <burhankozleme@gmail.com>
2024-10-25 21:52:29 +0300

    added main.h file

---
+++ Inc/main.h
@@ -0,0 +1,37 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/*
+ *  Common Library Data Type
+ */
+typedef enum
+{
+	OK,
+	ERROR,
+	BUSY
+
+}LibStatusFlag;
+
+
+/*
+ *  Microcontroller Library
+ */
+#include "stm32f4xx.h"
+#include "system_stm32f4xx.h"
+#include "stm32f407xx.h"
+
+/*
+ *  Core Library
+ */
+#include "cmsis_version.h"
+#include "cmsis_compiler.h"
+#include "cmsis_gcc.h"
+#include "core_cm4.h"
+
+/*
+ *  My Library
+ */
+#include "stm32f407_rcc.h"
+#include "stm32f407_gpio.h"
+
+#endif /* MAIN_H_ */
